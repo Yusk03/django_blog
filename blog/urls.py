a273d82421a7161fb2c4e7db3b7e66a47d0dc120
Yusk <psixopat1998@gmail.com>
2024-06-25 23:49:01 +0300

    added detailed info about news

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('topic/<int:topic_id>/', views.topic_news, name='topic_news'),
+    path('news/<int:news_id>/', views.news_detail, name='news_detail'),
 ]
