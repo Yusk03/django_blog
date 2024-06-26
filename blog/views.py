5a70597017eaa9aa565d4b24147ac23a84286e6c
Yusk <psixopat1998@gmail.com>
2024-06-26 13:58:22 +0300

    added db and photos to project

--- blog/views.py
+++ blog/views.py
@@ -13,6 +13,7 @@ def topic_news(request, topic_id):
     news = News.objects.filter(topic=topic).order_by('-date_published')
     return render(request, 'blog/topic_news.html', {'topic': topic, 'news': news})
 
+
 def news_detail(request, news_id):
     news_item = get_object_or_404(News, id=news_id)
     return render(request, 'blog/news_detail.html', {'news_item': news_item})
